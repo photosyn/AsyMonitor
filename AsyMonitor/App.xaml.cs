ebe565d05bbf49dcfa0c0ff827c3f00a0fa8cea9
photosyn <lwb0206@163.com>
2018-09-05 14:15:13 +0800

    1.程序只允许存在一个实例 2.增加程序图标 3.调整程序结构

--- AsyMonitor/App.xaml.cs
+++ AsyMonitor/App.xaml.cs
@@ -13,6 +13,24 @@ namespace AsyMonitor
     /// </summary>
     public partial class App : Application
     {
-       
+        System.Threading.Mutex mutex;
+
+        public App()
+        {
+            this.Startup += new StartupEventHandler(App_Startup);
+        }
+
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+            bool ret;
+            mutex = new System.Threading.Mutex(true, "AsyMonitor", out ret);
+
+            if (!ret)
+            {
+                MessageBox.Show("已有一个程序实例运行");
+                Environment.Exit(0);
+            }
+
+        }
     }
 }
